d7c2d012c8a2788e48a95564565bf764d8c93f22
Marcos Merlini <merlinimarcos@gmail.com>
2024-04-09 21:28:09 -0300

    created base screem

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+export function Header() {
+  return (
+      <div className='flex items-center gap-5 py-2'>
+          <nav className='flex items-center gap-5'>
+              <a
+                  href=''
+                  className='font-medium text-sm text-zinc-300'
+              >
+                  Eventos
+              </a>
+              <a
+                  href=''
+                  className='font-medium text-sm'
+              >
+                  Participantes
+              </a>
+          </nav>
+      </div>
+  );
+}
